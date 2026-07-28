6c36cd02b8a5ddbcbf3d43d5a9876939fc7b75b0
Konrad Michalik <km@move-elevator.de>
2026-07-28 08:46:21 +0200

    test: migrate EnvironmentHelperTest to WithEnvVar including unset support

--- Tests/Unit/Utility/EnvironmentHelperTest.php
+++ Tests/Unit/Utility/EnvironmentHelperTest.php
@@ -13,10 +13,9 @@ declare(strict_types=1);
 
 namespace KonradMichalik\Typo3DumpServer\Tests\Unit\Utility;
 
+use KonradMichalik\Ttt\Attribute\WithEnvVar;
 use KonradMichalik\Typo3DumpServer\Utility\EnvironmentHelper;
 use PHPUnit\Framework\TestCase;
-
-use function is_string;
 
 /**
  * EnvironmentHelperTest.
@@ -26,184 +25,118 @@ use function is_string;
  */
 final class EnvironmentHelperTest extends TestCase
 {
-    private string $originalEnvValue;
-
-    private string $originalIdeValue;
-
-    private string $originalPathMapValue;
-
-    private string $originalDdevAppRootValue;
-
-    protected function setUp(): void
-    {
-        $dumpServerHost = getenv('TYPO3_DUMP_SERVER_HOST');
-        $this->originalEnvValue = is_string($dumpServerHost) ? $dumpServerHost : '';
-
-        $dumpServerIde = getenv('TYPO3_DUMP_SERVER_IDE');
-        $this->originalIdeValue = is_string($dumpServerIde) ? $dumpServerIde : '';
-
-        $dumpServerPathMap = getenv('TYPO3_DUMP_SERVER_PATH_MAP');
-        $this->originalPathMapValue = is_string($dumpServerPathMap) ? $dumpServerPathMap : '';
-
-        $ddevAppRoot = getenv('DDEV_APPROOT');
-        $this->originalDdevAppRootValue = is_string($ddevAppRoot) ? $ddevAppRoot : '';
-    }
-
-    protected function tearDown(): void
-    {
-        if ('' !== $this->originalEnvValue) {
-            putenv('TYPO3_DUMP_SERVER_HOST='.$this->originalEnvValue);
-        } else {
-            putenv('TYPO3_DUMP_SERVER_HOST');
-        }
-
-        if ('' !== $this->originalIdeValue) {
-            putenv('TYPO3_DUMP_SERVER_IDE='.$this->originalIdeValue);
-        } else {
-            putenv('TYPO3_DUMP_SERVER_IDE');
-        }
-
-        if ('' !== $this->originalPathMapValue) {
-            putenv('TYPO3_DUMP_SERVER_PATH_MAP='.$this->originalPathMapValue);
-        } else {
-            putenv('TYPO3_DUMP_SERVER_PATH_MAP');
-        }
-
-        if ('' !== $this->originalDdevAppRootValue) {
-            putenv('DDEV_APPROOT='.$this->originalDdevAppRootValue);
-        } else {
-            putenv('DDEV_APPROOT');
-        }
-    }
-
+    #[WithEnvVar('TYPO3_DUMP_SERVER_HOST')]
     public function testGetHostReturnsDefaultWhenEnvironmentVariableNotSet(): void
     {
-        putenv('TYPO3_DUMP_SERVER_HOST');
-
         $host = EnvironmentHelper::getHost();
 
         self::assertSame('tcp://127.0.0.1:9912', $host);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_HOST', 'tcp://192.168.1.100:9999')]
     public function testGetHostReturnsEnvironmentVariableWhenSet(): void
     {
-        $customHost = 'tcp://192.168.1.100:9999';
-        putenv('TYPO3_DUMP_SERVER_HOST='.$customHost);
-
         $host = EnvironmentHelper::getHost();
 
-        self::assertSame($customHost, $host);
+        self::assertSame('tcp://192.168.1.100:9999', $host);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_HOST', '')]
     public function testGetHostReturnsEmptyStringWhenEnvironmentVariableIsEmpty(): void
     {
-        putenv('TYPO3_DUMP_SERVER_HOST=');
-
         $host = EnvironmentHelper::getHost();
 
         self::assertSame('', $host);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_IDE')]
     public function testGetIdeReturnsNullWhenEnvironmentVariableNotSet(): void
     {
-        putenv('TYPO3_DUMP_SERVER_IDE');
-
         $ide = EnvironmentHelper::getIde();
 
         self::assertNull($ide);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_IDE', 'phpstorm')]
     public function testGetIdeReturnsEnvironmentVariableWhenSet(): void
     {
-        putenv('TYPO3_DUMP_SERVER_IDE=phpstorm');
-
         $ide = EnvironmentHelper::getIde();
 
         self::assertSame('phpstorm', $ide);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_IDE', '')]
     public function testGetIdeReturnsNullWhenEnvironmentVariableIsEmpty(): void
     {
-        putenv('TYPO3_DUMP_SERVER_IDE=');
-
         $ide = EnvironmentHelper::getIde();
 
         self::assertNull($ide);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP')]
+    #[WithEnvVar('DDEV_APPROOT')]
     public function testGetPathMappingReturnsNullWhenNotSet(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP');
-        putenv('DDEV_APPROOT');
-
         self::assertNull(EnvironmentHelper::getPathMapping());
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '')]
+    #[WithEnvVar('DDEV_APPROOT')]
     public function testGetPathMappingReturnsNullWhenEmpty(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP=');
-        putenv('DDEV_APPROOT');
-
         self::assertNull(EnvironmentHelper::getPathMapping());
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '/var/www/html=/Users/me/Projects')]
     public function testGetPathMappingReturnsMappingWhenSet(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP=/var/www/html=/Users/me/Projects');
-
         $mapping = EnvironmentHelper::getPathMapping();
 
         self::assertSame(['/var/www/html', '/Users/me/Projects'], $mapping);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '/var/www/html')]
     public function testGetPathMappingReturnsNullWhenInvalidFormat(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP=/var/www/html');
-
         self::assertNull(EnvironmentHelper::getPathMapping());
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '=/Users/me/Projects')]
     public function testGetPathMappingReturnsNullWhenFromIsEmpty(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP==/Users/me/Projects');
-
         self::assertNull(EnvironmentHelper::getPathMapping());
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '/var/www/html=/Users/me/Pro=jects')]
     public function testGetPathMappingHandlesPathsWithEquals(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP=/var/www/html=/Users/me/Pro=jects');
-
         $mapping = EnvironmentHelper::getPathMapping();
 
         self::assertSame(['/var/www/html', '/Users/me/Pro=jects'], $mapping);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP')]
+    #[WithEnvVar('DDEV_APPROOT', '/Users/me/Sites/myproject')]
     public function testGetPathMappingFallsToDdevAppRoot(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP');
-        putenv('DDEV_APPROOT=/Users/me/Sites/myproject');
-
         $mapping = EnvironmentHelper::getPathMapping();
 
         self::assertSame(['/var/www/html', '/Users/me/Sites/myproject'], $mapping);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP', '/opt/app=/Users/me/custom')]
+    #[WithEnvVar('DDEV_APPROOT', '/Users/me/Sites/myproject')]
     public function testGetPathMappingPrefersExplicitOverDdev(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP=/opt/app=/Users/me/custom');
-        putenv('DDEV_APPROOT=/Users/me/Sites/myproject');
-
         $mapping = EnvironmentHelper::getPathMapping();
 
         self::assertSame(['/opt/app', '/Users/me/custom'], $mapping);
     }
 
+    #[WithEnvVar('TYPO3_DUMP_SERVER_PATH_MAP')]
+    #[WithEnvVar('DDEV_APPROOT', '')]
     public function testGetPathMappingIgnoresEmptyDdevAppRoot(): void
     {
-        putenv('TYPO3_DUMP_SERVER_PATH_MAP');
-        putenv('DDEV_APPROOT=');
-
         self::assertNull(EnvironmentHelper::getPathMapping());
     }
 }
